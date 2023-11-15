833e8033239eb395acdf94c4eca3ffbc037ca397
edgarmw <edgarmingwei@gmail.com>
2023-11-15 23:14:06 +0800

    added user authentication required

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -4,12 +4,14 @@ from rest_framework import generics, viewsets
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 #def index(request):
 #    return render(request, 'index.html', {})
 
 class MenuItemView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -20,6 +22,8 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+
     
